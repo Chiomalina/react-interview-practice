beed43f3ef0b220863d1ceb8bfb9f3ceb0216102
Chiomalina <chiomalinaanaso@gmail.com>
2025-12-22 05:35:17 +0100

    uncontrolled login using useRef hook

--- react-interview-practice/src/App.jsx
+++ react-interview-practice/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ControlledLivePreview from "./exercises/forms/controlled-live-preview/controlledLivePreview";
+import UncontrolledLoginUseRef from "./exercises/forms/uncontrolled-login-useref/UncontrolledLoginUseRef";
 
 function App() {
   return (
     <div>
       <ControlledLivePreview />
+      <UncontrolledLoginUseRef />
     </div>
   );
 }
---
+++ react-interview-practice/src/exercises/forms/uncontrolled-login-useref/UncontrolledLoginUseRef.jsx
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+
+function UncontrolledLoginUseRef() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    // Input validation
+    if (!email || !password) {
+      alert("Please enter your email address and password");
+      return;
+    }
+
+    console.log({ email, password });
+    alert("Form submitted sucessfull, check console");
+  }
+
+  return (
+    <div style={{ padding: 16 }}>
+      <h2>Uncontrolled Form (useRef)</h2>
+
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: 12 }}>
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" ref={emailRef} placeholder="Email" />
+        </div>
+
+        <div style={{ marginBottom: 12 }}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            ref={passwordRef}
+            placeholder="password"
+          />
+        </div>
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default UncontrolledLoginUseRef;

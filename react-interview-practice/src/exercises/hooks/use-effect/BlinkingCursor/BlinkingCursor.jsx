c0af5a0be7411e8ba33298e3ebf961a0978988e8
Chiomalina <chiomalinaanaso@gmail.com>
2025-12-29 04:39:14 +0100

    blinking cursor (advanced typewriter) with buttons control

--- react-interview-practice/src/exercises/hooks/use-effect/BlinkingCursor/BlinkingCursor.jsx
+++ react-interview-practice/src/exercises/hooks/use-effect/BlinkingCursor/BlinkingCursor.jsx
@@ -1,27 +1,86 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function BlinkingCursor({ text = "Hello Chioma", speed = 80 }) {
+export default function BlinkingCursor(props) {
+  // Destructure props
+  const { text = "Hello World", speed = 80 } = props;
+
+  // Display State
   const [index, setIndex] = useState(0);
+  const [isRunning, setIsRunning] = useState(true);
+
+  // Cursor state
+  const [showCursor, setShowCursor] = useState(true);
 
   const isDone = index >= text.length;
+  const typed = text.slice(0, index);
 
+  // 1) Typewriter effect (setTimeout and cleanup)
   useEffect(() => {
+    if (!isRunning) return;
     if (isDone) return;
 
     const timeoutId = window.setTimeout(() => {
       setIndex((prev) => prev + 1);
     }, speed);
 
-    // cleanup timeout
     return () => window.clearTimeout(timeoutId);
-  }, [text, speed, isDone, index]);
+  }, [index, text, isRunning, speed]);
+
+  // Restart typing when text changes
+  useEffect(() => {
+    setIndex(0);
+  }, [text]);
+
+  //2) Blinking cursor (setInterval + cleanup)
+  useEffect(() => {
+    const intervalId = window.setInterval(() => {
+      setShowCursor((prev) => !prev);
+    }, 500);
+
+    return () => window.clearInterval(intervalId);
+  }, []);
+
+  function handleStart() {
+    setIsRunning(true);
+  }
+
+  function handlePause() {
+    setIsRunning(false);
+  }
+
+  function handleReset() {
+    setIndex(0);
+    setIsRunning(true);
+  }
 
   return (
     <div>
-      <span>{text.slice(0, index)}</span>
-      <span>|</span>
+      <p>
+        {typed}
+        <span style={{ opacity: showCursor && !isDone ? 1 : 0 }}>|</span>
+      </p>
+
+      <div>
+        <button
+          style={{ backgroundColor: "green" }}
+          onClick={handleStart}
+          disabled={isRunning || isDone}
+        >
+          Start
+        </button>
+
+        <button
+          style={{ backgroundColor: "red" }}
+          onClick={handlePause}
+          disabled={isDone}
+        >
+          Pause
+        </button>
+
+        <button style={{ backgroundColor: "grey" }} onClick={handleReset}>
+          Reset
+        </button>
+      </div>
     </div>
   );
 }
-
-export default BlinkingCursor;

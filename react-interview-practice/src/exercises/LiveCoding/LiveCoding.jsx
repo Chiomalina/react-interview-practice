6fec477da59469ab0c750e7cec721dff0970ad6c
Chiomalina <chiomalinaanaso@gmail.com>
2026-01-04 04:47:38 +0100

    Live coding practice

--- react-interview-practice/src/exercises/LiveCoding/LiveCoding.jsx
+++ react-interview-practice/src/exercises/LiveCoding/LiveCoding.jsx
@@ -1,15 +1,19 @@
 // 1. Write a getType utility that correctly detect null, arrays, functions, primitives
-// 2. Only allow arrays 
+// 2. Only allow arrays
 
 function LiveCoding() {
-  function getType(value) {
-    if (value === null) return "null";
-    if (Array.isArray(value)) return "array";
-    return typeof value;
+  function getType(values) {
+    if (values === null) return "null";
+    if (Array.isArray(values)) return "arrays";
+    return typeof values;
   }
 
   function guardFunction(users) {
-
+    if (!Array.isArray(users)) {
+      console.log("Expected an array");
+      throw new Error("Expected an array");
+    }
+    return "Thanks";
   }
   return (
     <div>
@@ -17,7 +21,14 @@ function LiveCoding() {
       <div>getType([]): {getType([])}</div>
       <div>getType(null):{getType(null)}</div>
       <div>getType(42):{getType(42)}</div>
+      <div>
+        getType(dictionary):{getType({ name: "Mima" }) ? "true" : "false"}
+      </div>
       <div>getType(function):{getType(() => {})}</div>
+      <div>Guarding function argument</div>
+      <div>
+        guardFunction({}): {guardFunction({})}
+      </div>
     </div>
   );
 }

a01331d3fd4c821c5ea9eb167f0ada53333aa502
Chiomalina <chiomalinaanaso@gmail.com>
2026-01-05 14:28:01 +0100

    write todoRemover

--- react-interview-practice/src/exercises/LiveCoding/TodoRemover/TodoRemover.jsx
+++ react-interview-practice/src/exercises/LiveCoding/TodoRemover/TodoRemover.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { Toast } from "bootstrap";
+import { useState } from "react";
 
 function TodoRemover() {
   // Set todos state and initialize with a list of few items
@@ -12,23 +13,25 @@ function TodoRemover() {
   const [text, setText] = useState("");
 
   function handleChange(event) {
-    event.preventDefault();
+    event.preventDefault;
     setText(event.target.value);
-
-    // clean up text
-    setText("");
   }
 
-  function handleAdd(id) {
+  function handleAdd() {
     const trimmedText = text.trim();
     if (!trimmedText) return;
 
-    const newText = { id: Date.now, text: trimmedText };
+    const newText = { id: Date.now(), text: trimmedText };
     setTodos((prev) => [...prev, newText]);
+
+    // clean up text
+    setText("");
+    alert(`Todo witht the id ${newText.id} has been successfully added`);
   }
 
   function handleRemove(id) {
     setTodos((prev) => prev.filter((todo) => todo.id !== id));
+    alert(`todo with the id ${id} has been sucessfully deleted`);
   }
 
   return (
@@ -41,13 +44,27 @@ function TodoRemover() {
           type="text"
           value={text}
           placeholder="Type your todos here."
-          onClick={handleChange}
+          onChange={handleChange}
         />
 
-        <button onClick={handleAdd}>Add</button>
+        <button onClick={() => handleAdd()}>Add</button>
       </div>
 
       {/** Manage Removal button */}
+      {todos.length == 0 ? (
+        <p>You have no todo's</p>
+      ) : (
+        <ul>
+          {todos.map((todo) => (
+            <div>
+              <li key={todo.id}>{todo.text}</li>{" "}
+              <button onClick={() => handleRemove(todo.id)}>Remove</button>
+            </div>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
+
+export default TodoRemover;

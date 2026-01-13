e06078e91d9b538f330458b85ea63639671fc804
Chiomalina <chiomalinaanaso@gmail.com>
2026-01-13 05:29:12 +0100

    practice roman to integer leetcode task

--- react-interview-practice/src/LiveInterview/RomanToInteger.js
+++ react-interview-practice/src/LiveInterview/RomanToInteger.js
@@ -23,14 +23,36 @@ X can be placed before L (50) and C (100) to make 40 and 90.
 C can be placed before D (500) and M (1000) to make 400 and 900.
 Given a roman numeral, convert it to an integer.
 
+Example 1:
+
+Input: s = "III"
+Output: 3
+Explanation: III = 3.
+Example 2:
+
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+Example 3:
+
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+Constraints:
+
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999]
 
 
 
-    Stap by Step Strategy:
-    a) Create a mapping of Roman symbols to values.
-    b) Loop through the string from left to right.
-    c) Compare the current symbol with the next symbol.
-    d) Decide to add or subtract.
-    e) Return the final sum.    
+
+Step by Step Strategy:
+a) Create a mapping of Roman symbols to values.
+b) Loop through the string from left to right.
+c) Compare the current symbol with the next symbol.
+d) Decide to add or subtract.
+e) Return the final sum.    
     
 */}
